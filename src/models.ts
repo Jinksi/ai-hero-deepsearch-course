c8db7614fd8e8d0452009f3df078211a1cb6d9b8
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2025-07-21 06:57:39 +1000

    day 5: relevancy scorer

--- src/models.ts
+++ src/models.ts
@@ -4,3 +4,6 @@ import { openai } from "@ai-sdk/openai";
 export const model = openai("gpt-4.1-mini-2025-04-14");
 
 export const factualityModel = openai("gpt-4.1-mini-2025-04-14");
+
+export const statementGenerationModel = openai("gpt-4.1-nano-2025-04-14");
+export const answerRelevancyModel = openai("gpt-4.1-mini-2025-04-14");
